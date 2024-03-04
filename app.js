fcb8e27e80bd76dc37f53d5fe5fd96e0b306e87b
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-03-04 22:59:48 +0600

    function

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('connect')
+const allBtn = document.getElementsByClassName('add-btn');
+
+let count = 0;
+for(const btn of allBtn){
+    btn.addEventListener('click',function(event){
+         count ++;
+         const placeName = event.target.parentNode.childNodes[1].innerText;
+         const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+         
+
+         const selectedContainer = document.getElementById('selected-place-container');
+
+         const li = document.createElement('li');
+         const p = document.createElement('p');
+         p.innerText = placeName;
+         const p2 = document.createElement('p');
+         p2.innerText = price;
+
+         li.appendChild(p) 
+         li.appendChild(p2) 
+
+         selectedContainer.appendChild(li);
+
+
+        setInnerText('cart-count',count);
+    })
+}
+
+function setInnerText(id,value){
+    document.getElementById(id).innerText = value;
+    // console.log(id,value)
+}
